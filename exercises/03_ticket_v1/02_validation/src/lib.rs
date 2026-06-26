c4da98d42218dddff8913513d07daa63f9ced169
Toraja <Toraja@users.noreply.github.com>
2026-06-26 21:05:23 +0900

    03_ticket_v1/02_validation

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -5,7 +5,7 @@ struct Ticket {
 }
 
 impl Ticket {
-    // TODO: implement the `new` function.
+    // implement the `new` function.
     //  The following requirements should be met:
     //   - Only `To-Do`, `In Progress`, and `Done` statuses are allowed.
     //   - The `title` and `description` fields should not be empty.
@@ -18,11 +18,41 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        Self::validate_title(&title);
+        Self::validate_description(&description);
+        Self::validate_status(&status);
+
         Self {
             title,
             description,
             status,
+        }
+    }
+
+    fn validate_title(title: &str) {
+        if title.trim().is_empty() {
+            panic!("Title cannot be empty");
+        }
+
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+    }
+
+    fn validate_description(description: &str) {
+        if description.trim().is_empty() {
+            panic!("Description cannot be empty");
+        }
+
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+    }
+
+    fn validate_status(status: &str) {
+        let statuses = ["To-Do", "In Progress", "Done"];
+        if !statuses.contains(&status) {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
     }
 }
